7f1a442975201137cfc9aa3ef0438d96bc8e9aa2
cdixson-ds <crystal.dixson@gmail.com>
2020-09-01 15:40:15 -0700

    Add files via upload
    updated assignment

---
+++ src/03_modules.py
@@ -0,0 +1,45 @@
+"""
+In this exercise, you'll be playing around with the sys module,
+which allows you to access many system specific variables and
+methods, and the os module, which gives you access to lower-
+level operating system functionality.
+"""
+
+import sys
+#from sys import platform
+# See docs for the sys module: https://docs.python.org/3.7/library/sys.html
+
+# Print out the command line arguments in sys.argv, one per line:
+# YOUR CODE HERE
+
+print("name of the script: ", sys.argv[0])
+print("number of arguments: ", len(sys.argv))
+print("arguments: ", str(sys.argv))
+
+# Print out the OS platform you're using:
+# YOUR CODE HERE
+
+print("OS platform: " , sys.platform)
+
+# Print out the version of Python you're using:
+# YOUR CODE HERE
+
+print("Python version: ", sys.version)
+
+import os
+# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+
+# Print the current process ID
+# YOUR CODE HERE
+
+print("process ID: ", os.getpid())
+
+# Print the current working directory (cwd):
+# YOUR CODE HERE
+
+print("current working directory: ", os.getcwd())
+
+# Print out your machine's login name
+# YOUR CODE HERE
+
+print("machine's login: ", os.getlogin())
---
+++ src/13_file_io.py
@@ -0,0 +1,34 @@
+"""
+Python makes performing file I/O simple. Take a look
+at how to read and write to files here:
+
+https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+"""
+
+# Open up the "foo.txt" file (which already exists) for reading
+# Print all the contents of the file, then close the file
+# Note: pay close attention to your current directory when trying to open "foo.txt"
+
+# YOUR CODE HERE
+
+foo = open('C:\\Users\\cryst\\Desktop\\Intro-Python\\src\\foo.txt', 'r')
+print(foo.read())
+foo.close()
+
+# Open up a file called "bar.txt" (which doesn't exist yet) for
+# writing. Write three lines of arbitrary content to that file,
+# then close the file. Open up "bar.txt" and inspect it to make
+# sure that it contains what you expect it to contain
+
+# YOUR CODE HERE
+
+bar = open('C:\\Users\\cryst\\Desktop\\Intro-Python\\src\\bar.txt', 'w')
+
+bar.write((
+    'But if you do, sir, I am for you. I serve as good a man as you.\n'
+    'No better.\n'
+    'Well, sir.\n'
+    'Say better. Here comes one of my masters kinsmen.\n'
+
+))
+
